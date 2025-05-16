dc56dcfa6c09e20b12230752bb275b6f82fa8aa8
CharquitodeLodo <carlos.moreno@keyinstitute.edu.sv>
2025-05-16 14:54:55 -0600

    Add files via upload

---
+++ Trabajo de Clase 1.py
@@ -0,0 +1,33 @@
+#'''
+#Clase:        Fase de Fortalecimiento Lógico 1
+#Tema:         Tipos de Variables
+#Ejercicio:    Automatizando el cálculo de la propina
+#Descripción:  Definición del ejercicio
+
+#Autor:        Carlos Eduardo Moreno Díaz
+#Fecha:        2025-05-15
+#Estado:       [ En progreso | Terminado ]
+#'''
+total = float(input("Ingrese el balor de su cuneta"))
+p = float(input("que porcentaje de propina quiere?"))
+porcentaje = (total*p)/100
+print(f"La propina es {porcentaje}")
+total_a_pagar = total + porcentaje
+print(f"El total a pagar es {total_a_pagar}")
+
+#'''
+#Clase:        Fase de Fortalecimiento Lógico 1
+#Tema:         Tipos de Variables
+#Ejercicio:    Automatizando el cálculo de la propina
+#Descripción:  Definición del ejercicio
+
+#Autor:        Carlos Eduardo Moreno Díaz
+#Fecha:        2025-05-15
+#Estado:       [ En progreso | Terminado ]
+#'''
+print("Ingrese su nombre completo")
+primero = input()
+segundo = input()
+tercero = input()
+cuarto = input()
+print(f"Su nombre es {primero}_{cuarto}@keyinstitute.edu.sv")
